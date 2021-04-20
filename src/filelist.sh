06bceed33340d1f91839eedfd8187bfb977592dc
Charalampos Kardaris <ckardaris@outlook.com>
2021-04-20 15:48:44 +0300

    Fix redirection

--- src/filelist.sh
+++ src/filelist.sh
@@ -35,7 +35,7 @@ read_filenames() {
     esac
     if [[ "$#" -gt 0 ]]
     then
-        mapfile -t filelist < <(ls $ls_args "$@" &>/dev/null)
+        mapfile -t filelist < <(ls $ls_args "$@" 2>/dev/null)
     else
         mapfile -t filelist < <(ls $ls_args -- *)
     fi
